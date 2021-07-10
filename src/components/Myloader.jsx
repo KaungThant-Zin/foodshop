4d61c795e5014cf0496044e94f7d51020b06d689
KaungThant-Zin <mrxray261@gmail.com>
2021-07-10 19:34:12 +0630

    loading screen

--- src/components/Myloader.jsx
+++ src/components/Myloader.jsx
@@ -3,8 +3,8 @@ import ReactLoading from 'react-loading';
 
 const Myloader = () => {
     return (
-        <div className="flex justify-center text-center items-center mt-20 pt-20">
-            <ReactLoading type={"bars"} color={'#f79400'} height={50} width={50} />
+        <div className="flex justify-center text-center items-center mt-56">
+            <ReactLoading type={"bars"} color={'#f79400'} height={70} width={70} />
         </div>
     )
 }
