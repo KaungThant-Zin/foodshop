040c5ba6e6505127359db9438c66af31603f0539
KaungThant-Zin <mrxray261@gmail.com>
2021-07-10 11:42:08 +0630

    fixed about order

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -34,24 +34,17 @@ const About = () => {
             </div>
             <div className="grid lg:grid-cols-2 items-center">
                 {/* formobile */}
-                <div className="p-3 sm:block hidden">
+                <div className="p-3 lg:order-1 order-2">
                     <Zoom><h1 className="mt-2 text-5xl headfont foodcolor border-b-2 mx-3">Who we are</h1></Zoom>
                     <Zoom>
                     <p className="mt-3 mx-3 italic text-justify navfont">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est beatae magni nam rem deserunt quasi dignissimos, doloremque minus asperiores excepturi dicta! Minima cupiditate dolore consequuntur maiores aperiam ea, aspernatur nobis! Lorem ipsum dolor sit amet consectetur adipisicing elit. At, sint! Necessitatibus eveniet labore assumenda, magnam natus sed obcaecati ad ab magni odio, nesciunt nisi commodi! Nulla ratione eaque voluptates expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum nihil fugiat dolore? Blanditiis, earum beatae porro magnam saepe non sequi numquam modi quibusdam? Dolores autem accusantium quos odit ipsam minus.</p>
                     </Zoom>
                 </div>
                 <Zoom>
-                <div className="p-3 m-3 text-center">
+                <div className="p-3 m-3 text-center sm:order-1">
                     <img src={whoweare} alt="whoweare" className="rounded-3xl object-cover w-full" />
                 </div>
                 </Zoom>
-                {/* fordesktop */}
-                <div className="p-3 lg:hidden md:hidden">
-                    <Zoom><h1 className="mt-2 text-5xl headfont foodcolor border-b-2 mx-3">Who we are</h1></Zoom>
-                    <Zoom>
-                    <p className="mt-3 mx-3 italic text-justify navfont">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est beatae magni nam rem deserunt quasi dignissimos, doloremque minus asperiores excepturi dicta! Minima cupiditate dolore consequuntur maiores aperiam ea, aspernatur nobis! Lorem ipsum dolor sit amet consectetur adipisicing elit. At, sint! Necessitatibus eveniet labore assumenda, magnam natus sed obcaecati ad ab magni odio, nesciunt nisi commodi! Nulla ratione eaque voluptates expedita? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum nihil fugiat dolore? Blanditiis, earum beatae porro magnam saepe non sequi numquam modi quibusdam? Dolores autem accusantium quos odit ipsam minus.</p>
-                    </Zoom>
-                </div>
             </div>
         </div>
     )
