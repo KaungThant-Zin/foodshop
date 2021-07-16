10429ba26952ba12eafbf6d49f2626b6cc2610c8
KaungThant-Zin <mrxray261@gmail.com>
2021-07-16 17:38:05 +0630

    small img in sm

--- src/components/Homeitems2.jsx
+++ src/components/Homeitems2.jsx
@@ -5,7 +5,7 @@ import logofood from '../assets/food4.png'
 const Homeitems2 = () => {
     return (
         <>
-            <div className=" lg:my-10 p-3 flex justify-center sm:order-1">
+            <div className=" lg:my-10 px-10 lg:p-3 mt-4 md:p-3 flex justify-center sm:order-1">
                     <div className="imgshape">
                         <Flip>
                             <img src={logofood} alt="logofood" className="object-cover foodlogo rounded-full" />
